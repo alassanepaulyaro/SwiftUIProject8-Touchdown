778e69cc17546626e70debebbcd445cab679824f
Paul Yaro <alassane-paul.yaro@outlook.fr>
2024-12-16 08:56:52 +0100

    Initial Commit

---
+++ SwiftUIProject8-Touchdown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIProject8-Touchdown
+//
+//  Created by Yaro Paul  on 16/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIProject8-Touchdown/SwiftUIProject8_TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProject8_TouchdownApp.swift
+//  SwiftUIProject8-Touchdown
+//
+//  Created by Yaro Paul  on 16/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProject8_TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
